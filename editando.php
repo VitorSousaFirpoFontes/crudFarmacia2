e35abc5589cb5d0eff75806ff6cef1d6d40346c1
VitorSousaFirpoFontes <vitor.fontes@ba.estudante.senai.br>
2024-10-17 20:48:53 -0300

    commit

--- editando.php
+++ editando.php
@@ -25,7 +25,7 @@ $dados = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($dados) > 0) {
  
-    echo "<table>";
+    echo "<table class="table">";
     echo "<tr><th>ID</th><th>Nome</th><th>Descricao</th><th>Senha</th></tr>"; 
 
     while ($linha = mysqli_fetch_assoc($dados)) {
@@ -40,7 +40,7 @@ if (mysqli_num_rows($dados) > 0) {
     echo "</tr>";  
 }
 }
-
+    
 
 
 
@@ -52,6 +52,9 @@ if (mysqli_num_rows($dados) > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 </head>
 <link href="style.css" rel="stylesheet" />
 <body>
--- index.php
+++ index.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Produto</title>
     <link href="style.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 </head>
-<section>
 <body>
-    <h1>Cadastro de medicamentos</h1>
-    <br> 
-   
-    <form action="cadastro_script.php" method="POST" class="form-centralizado">
-        <label for="nome">Nome do medicamento:</label>
-        <input class="meuinput" type="text" id="nome" name="nome" required><br><br>
+    <section>
+        <h1>Cadastro de Medicamentos</h1>
+        <br> 
+        <form action="cadastro_script.php" method="POST" class="form-centralizado">
+            <label for="nome">Nome do medicamento:</label>
+            <input class="meuinput" type="text" id="nome" name="nome" required>
 
-        <label for="descricao">Preço unitário:</label>
-        <input class="meuinput" type="number" id="preco" name="preco" required></input><br><br>
+            <label for="preco">Preço unitário:</label>
+            <input class="meuinput" type="number" id="preco" name="preco" required>
 
-        <label for="quantidade">Quantidade disponivel:</label>
-        <input class="meuinput" type="number" step="0.01" id="quantidade" name="quantidade" required><br><br>
+            <label for="quantidade">Quantidade disponível:</label>
+            <input class="meuinput" type="number" step="0.01" id="quantidade" name="quantidade" required>
 
-        <label for="quantidade">Categoria do remedio:</label>
-        <input class="meuinput" type="text" id="categoria" name="categoria" required><br><br>
-        <label for="quantidade">Data de validade:</label>
-        <input class="meuinput" type="date" id="validade" name="validade" required><br><br>
-        <input type="submit" value="Cadastrar medicamento">
-    </form>
-    <form action="pesquisar.php" method="GET">
-    <input type="submit" value="Pesquisar" class="btn-pesquisar" />
-    </form>
-</section>
+            <label for="categoria">Categoria do remédio:</label>
+            <input class="meuinput" type="text" id="categoria" name="categoria" required>
+
+            <label for="validade">Data de validade:</label>
+            <input class="meuinput" type="date" id="validade" name="validade" required>
+
+            <input type="submit" value="Cadastrar medicamento" class="btn-pesquisar">
+        </form>
+        <form action="pesquisar.php" method="GET" class="form-centralizado">
+            <input type="submit" value="Pesquisar" class="btn-pesquisar" />
+        </form>
+    </section>
 </body>
 </html>
